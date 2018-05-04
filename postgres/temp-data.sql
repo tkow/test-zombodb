971e97ac10cb4ac4a494c24e02b82d543c4b9694
tkow <t.kusama0613@outlook.jp>
2018-05-04 15:47:06 +0900

    elastic search のplugin 入れる

---
+++ postgres/temp-data.sql
@@ -0,0 +1,3 @@
+insert into products (id,name,keywords,short_summary,long_description,price,inventory_count,discontinued,availability_date)
+values (1,'test','{atype}','atype data','atype data for zombodb',1000,1,false,now());
+
